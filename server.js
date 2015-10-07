4abd81b3796df6f3d4cb52234ea90a7747904fd2
Binoy Patel <binoypatel14@gmail.com>
2015-10-07 16:05:59 -0400

    Fixed static routes

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/mean-todo');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 var apiRouter = require('./routes/api.js')(app, express);
 app.use('/api', apiRouter);
 
-app.use('*', function(req, res){
-  //res.sendFile('404.html');
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/404.html'));
 });
 
 app.listen(3000);
